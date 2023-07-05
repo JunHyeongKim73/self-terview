337a75e3449fba022e754be4c71cf2c940b712f9
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-06 00:21:55 +0900

    feat: 메인 UI를 위한 자바스크립트 작성

---
+++ static/index.js
@@ -0,0 +1,58 @@
+const videoElement = document.getElementById('videoElement');
+const previewElement = document.getElementById('previewElement');
+const startRecordButton = document.getElementById('startRecord');
+const stopRecordButton = document.getElementById('stopRecord');
+const downloadButton = document.getElementById('downloadRecord');
+
+let mediaRecorder;
+let recordedChunks = [];
+
+async function startCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+    videoElement.srcObject = stream;
+
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = function (event) {
+        if (event.data.size > 0) {
+            recordedChunks.push(event.data);
+        }
+    };
+
+    mediaRecorder.onstop = function () {
+        const blob = new Blob(recordedChunks, {type: 'video/webm'});
+        const url = URL.createObjectURL(blob);
+
+        previewElement.src = url;
+        downloadButton.addEventListener('click', () => {
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'recorded-video.webm';
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(() => {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 100);
+        });
+
+        downloadButton.disabled = false;
+    };
+}
+
+startRecordButton.addEventListener('click', () => {
+    mediaRecorder.start();
+    startRecordButton.disabled = true;
+    stopRecordButton.disabled = false;
+});
+
+stopRecordButton.addEventListener('click', () => {
+    mediaRecorder.stop();
+    startRecordButton.disabled = false;
+    stopRecordButton.disabled = true;
+    recordedChunks = [];
+});
+
+
+startCamera();
